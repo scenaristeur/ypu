ab909c30f4a6acdd52a5a4bd8794f1f1f26cd4d9
scenaristeur <scenaristeur@gmail.com>
2022-11-23 21:19:34 +0100

    list

--- js/about.3167c641.js
+++ js/about.3167c641.js
@@ -1,2 +1,2 @@
 "use strict";(self["webpackChunkypu"]=self["webpackChunkypu"]||[]).push([[443],{7381:function(n,u,t){t.r(u),t.d(u,{default:function(){return l}});var a=t(3396);const e={class:"about"},r=(0,a._)("h1",null,"This is an about page",-1),s=[r];function c(n,u){return(0,a.wg)(),(0,a.iD)("div",e,s)}var i=t(89);const o={},f=(0,i.Z)(o,[["render",c]]);var l=f}}]);
-//# sourceMappingURL=about.1ea91347.js.map
+//# sourceMappingURL=about.3167c641.js.map
